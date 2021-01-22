c7247cd1fc097096706eefadc3778cdae45e5445
Alonso Otero <alonsooteroseminario@gmail.com>
2021-01-22 20:48:40 -0300

    actualizado clase 3 NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,19 @@
-function App() {
-    return <>
-        <NavBar />
-        <h2>Las ofertas de la semana</h2>
-        </>;
+import React from 'react';
+
+const HolaMundo = () => {
+  // Esto es JavaScript:
+  const claseCSSHolaMundo = 'container-red';
+  const mensajeTextoHTML = 'Tienda Marketplace Webclash';
+
+  // Esto es JSX (HTML + JavaScript):
+  return (
+    <nav className={claseCSSHolaMundo}>
+      <h1>{mensajeTextoHTML}</h1>
+      <p>1. Tuberias.</p>
+      <p>2. Ductos.</p>
+      <p>3. Equipos Elecricos</p>
+    </nav>
+  );
 }
+
+export default HolaMundo;
