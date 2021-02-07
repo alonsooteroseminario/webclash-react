35d383f1b53ebc71235a173437f1d5b003687718
Alonso Otero <alonsooteroseminario@gmail.com>
2021-02-07 00:18:24 -0300

    actualizado sabado por la noche

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -4,31 +4,53 @@ function Item ({ item }) {
     const Resultado = new Promise( (resolve, reject) => {
         setTimeout( () => {
             resolve('Se ejecuto la funcion');
+            setUsers( [
+              {
+                id: 1,
+                title: "naranja",
+                price: 20,
+                pictureUrl: "http://www.azspagirls.com/files/2010/09/orange.jpg",
+              },
+              {
+                id: 2,
+                title: "platano",
+                price: 32,
+                pictureUrl: "http://images.all-free-download.com/images/graphicthumb/vector_illustration_of_ripe_bananas_567893.jpg",
+              },      {
+                id: 3,
+                title: "limón",
+                price: 51,
+                pictureUrl: "https://3.imimg.com/data3/IC/JO/MY-9839190/organic-lemon-250x250.jpg",
+              },
+            ] );
         }, 3000);
+
+
     })
 
     Resultado.then( e => {
         console.log(e);
     })
 
+    const [users, setUsers] = useState([]);
+
 
     return (
-        <div>
-            <section>
-              <div className="container">
-                <div className="row">
-                  <div className="col-lg-6 col-md-6 sm-mt-5">
-                    <h3>Item numero 1</h3>
-                  </div>
-                  <div className="col-lg-6 col-md-6 mt-5 mt-lg-0">
-                    <div className="product-details">
-                    <p>Item numero 2</p>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+              {users.map( el => 
+                    <div className="card" >
+                        <img src={el.pictureUrl} className="card-img-top" alt="Card image cap" ></img>
+                        <div className="card-block" key={el.id} >
+                            <h4 className="card-title">{`${el.title}: $${el.price}`}</h4>
+                            <input className="add-to-cart btn btn-primary" type="submit" value="Agregar" />
+                        </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </section>
+              )}
+          </div>
         </div>
+      </div>
     )
 }
 
--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Item from './Item';
 
 function ItemList ({ items }) {
+
     return (
         <Item />
     )
 }
+
+
 
 export default ItemList;
