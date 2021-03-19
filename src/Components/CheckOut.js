f844e6a4bf7c14ae62c783328277a9dfba8435d5
Alonso Otero <alonsooteroseminario@gmail.com>
2021-03-19 16:24:10 -0300

    traduccion ok

--- src/Components/CheckOut.js
+++ src/Components/CheckOut.js
@@ -20,6 +20,7 @@ const CheckOut = () => {
     const [lastName, setLastName] = useState('');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
+    const [emailConfirmado, setEmailConfirmado] = useState('');
     const [nombreCompleto, setNombreCompleto] = useState('');
 
     const [carTotal, setCartTotal] = useState(0);
@@ -39,86 +40,56 @@ const CheckOut = () => {
     const total = () => {
         let totalVal = 0;
         for (let i = 0; i < cart.length; i++) {
-            // alert(Object.values(cart[i].item.description));
             totalVal = totalVal + (Number(cart[i].quant) * Number(cart[i].item.price));
-            
         }
         setCartTotal(totalVal);
     };
 
     const finalizarCompra = async () => {
 
-        setLoading(true);
-        
-        // alert(nombreCompleto);
+        if (emailConfirmado === email) {
+            
+            setLoading(true);
+
+            const baseDeDatos = getFirestore();
+            const orders = baseDeDatos.collection('orders');
     
-        debugger;
+            let newOrder = {
+                buyer: { nombreCompleto , phone, email },
+                items: [...cart],
+                date: firebase.firestore.Timestamp.fromDate(new Date()),
+                total: carTotal
+            };
+    
+            console.log(newOrder);
+    
+            orders.add(newOrder).then( (or) => {
+                let id = or.id;
+                console.log(id);
+                console.log(or);
+                setOrderId(id);
+    
+            }).catch( (err) => {
+                setError(err);
+    
+            }).finally( () => {
+    
+            });
+    
+            console.log(orders);
 
-        const baseDeDatos = getFirestore();
-
-        const orders = baseDeDatos.collection('orders');
-
-        let newOrder = {
-            buyer: { nombreCompleto , phone, email },
-            items: [...cart],
-            date: firebase.firestore.Timestamp.fromDate(new Date()),
-            total: carTotal
-        };
-
-        console.log(newOrder);
-
-        // console.log(newOrder);
-        alert(newOrder.buyer.nombreCompleto.toString());
-
-        // orders.add(newOrder);
-
-        orders.add(newOrder).then( (or) => {
-            let id = or.id;
-            console.log(id);
-            console.log(or);
-            debugger;
-            // alert(or.id)
-            setOrderId(id);
-            // alert(order);
-        }).catch( (err) => {
-            setError(err);
-            // alert(error)
-        }).finally( () => {
-            // setLoading(false);
-        });
-
-        console.log(orders);
+        }
+        else{
+            alert("Los emails no coinciden. Escribelos correctamente por favor para poder continuar con la compra")
+        }
 
     };
-
-
-
-    // var docRef = db.collection('items').doc(itemId);
-
-    // var actualizarStock = docRef.update({
-    //     stock: stockPrevio -cantidadComprar
-    // });
-
-    // const batch = db.batch();
-
-    // batch.update(doc1, {field: 'newFieldValue'});
-    // batch.set(doc2, {field: 'newFieldValue'});
-
-    // batch.commit().then(r => r);
-
-
 
     return (
 
         <CartProvider>
             <div>
                 <div>
-                    {/*hero section start*/}
-                    <section className="position-relative">
-                    </section>
-                    {/*hero section end*/}
-
-
                     {/*body content start*/}
                     <div className="page-content">
                         { loading !== true
@@ -131,33 +102,38 @@ const CheckOut = () => {
                                     <div className="row">
                                         <div className="col-lg-7 col-md-12">
                                             <div className="checkout-form box-shadow white-bg">
-                                                <h2 className="mb-4">Billing Details</h2>
+                                                <h2 className="mb-4">Detalles de la compra</h2>
                                                 <form className="row">
                                                     <div className="col-md-6">
                                                         <div className="form-group">
-                                                            <label>First Name</label>
-                                                            <input type="text" id="fname" className="form-control" placeholder="Your firstname" onChange={ e => setFirstName(e.target.value)}/>
+                                                            <label>Nombres</label>
+                                                            <input type="text" id="fname" className="form-control" placeholder="Nano" onChange={ e => setFirstName(e.target.value)}/>
                                                             <span className="error"></span> 
                                                         </div>
                                                     </div>
                                                     <div className="col-md-6">
                                                         <div className="form-group">
-                                                            <label>Last Name</label>
-                                                            <input type="text" id="lname" className="form-control" placeholder="Your lastname" onChange={ e => setLastName(e.target.value)}/>
+                                                            <label>Apellidos</label>
+                                                            <input type="text" id="lname" className="form-control" placeholder="Perez" onChange={ e => setLastName(e.target.value)}/>
                                                             <span className="error"></span>
                                                         </div>
                                                     </div>
                                                     <div className="col-md-6">
                                                         <div className="form-group">
-                                                            <label>E-mail Address</label>
-                                                            <input type="text" id="email" className="form-control" placeholder="State Province" onChange={ e => setEmail(e.target.value)} />
+                                                            <label>Correo electrónico</label>
+                                                            <input type="text" id="email" className="form-control" placeholder="ejemplo@gmail.com" onChange={ e => setEmail(e.target.value)} />
+                                                            <span className="error"></span>
+                                                        </div>
+                                                        <div className="form-group">
+                                                            <label>Confirma el mismo Correo electrónico</label>
+                                                            <input type="text" id="email" className="form-control" placeholder="ejemplo@gmail.com" onChange={ e => setEmailConfirmado(e.target.value)} />
                                                             <span className="error"></span>
                                                         </div>
                                                     </div>
                                                     <div className="col-md-6">
                                                         <div className="form-group">
-                                                            <label>Phone Number</label>
-                                                            <input type="text" id="phone" className="form-control" placeholder onChange={ e => setPhone(e.target.value)}/>
+                                                            <label>Número de teléfono</label>
+                                                            <input type="text" id="phone" className="form-control" placeholder="+5695656567" onChange={ e => setPhone(e.target.value)}/>
                                                             <span className="error"></span>
                                                         </div>
                                                     </div>
@@ -175,45 +151,44 @@ const CheckOut = () => {
                                                         <li className="mb-3 border-bottom pb-3"><span>  {el.item.title}  </span> $ </li>
                                                         ))}
                                                         <li className="mb-3 border-bottom pb-3"><span> Shipping </span> $ 0.00</li>
-                                                        <li className="mb-3 border-bottom pb-3"><span> Subtotal </span> $ </li>
                                                         <li><span><strong className="cart-total"> Total :</strong></span>  <strong className="cart-total">$ {carTotal} </strong>
                                                         </li>
                                                     </ul>
 
                                                     :  
 
-                                                    <div>No Items found</div>
+                                                    <div>No se encuentran elementos</div>
 
                                                 }
                                             </div>
                                             <div className="cart-detail my-5">
-                                                <h3 className="mb-3">Payment Method</h3>
+                                                <h3 className="mb-3">Método de Pago</h3>
                                                 <div className="form-group">
                                                     <div className="custom-control custom-radio">
                                                         <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input" />
-                                                        <label className="custom-control-label" htmlFor="customRadio1">Direct Bank Tranfer</label>
+                                                        <label className="custom-control-label" htmlFor="customRadio1">Transferencia bancaria directa</label>
                                                     </div>
                                                 </div>
                                                 <div className="form-group">
                                                     <div className="custom-control custom-radio">
                                                         <input type="radio" id="customRadio2" name="customRadio" className="custom-control-input" />
-                                                        <label className="custom-control-label" htmlFor="customRadio2">Check Payment</label>
+                                                        <label className="custom-control-label" htmlFor="customRadio2">Tarjeta de crédito</label>
                                                     </div>
                                                 </div>
                                                 <div className="form-group">
                                                     <div className="custom-control custom-radio">
                                                         <input type="radio" id="customRadio3" name="customRadio" className="custom-control-input" />
-                                                        <label className="custom-control-label" htmlFor="customRadio3">Paypal Account</label>
+                                                        <label className="custom-control-label" htmlFor="customRadio3">Cuenta de Paypal</label>
                                                     </div>
                                                 </div>
                                                 <div className="form-group mb-0">
                                                     <div className="custom-control custom-checkbox">
                                                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                                                        <label className="custom-control-label" htmlFor="customCheck1">I have read and accept the terms and conditions</label>
+                                                        <label className="custom-control-label" htmlFor="customCheck1">He leído y acepto los terminos y condiciones del servicio.</label>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <button className="btn btn-primary btn-block" onClick={ () => { finalizarCompra() }}>Proceed to Payment</button>
+                                            <button className="btn btn-primary btn-block" onClick={ () => { finalizarCompra() }}>Proceder al pago</button>
                                         </div>
                                     </div>
                                 </form>
