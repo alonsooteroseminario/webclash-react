aa05cf5072561906d7028881313b8b447f9c7f5f
Alonso Otero <alonsooteroseminario@gmail.com>
2021-03-19 18:20:03 -0300

    cart widget ok

--- src/Components/CartWidget.js
+++ src/Components/CartWidget.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useContext, useState, useEffect } from 'react';
+import { CartContext } from '../context/CartContext';
 
 const CartWidget = () => {
+
+  const {cart} = useContext(CartContext);
+
+  let elements = 0;
+  for (let index = 0; index <cart.length; index++) {
+    elements = elements + Number(cart[index].quant);
+  }
+
   return (
     <div>
-      <span><i className="shopping-cart"></i></span>
+      { cart.length === 0 
+        ?
+        <div>
+          <span>
+            <i className="shopping-cart"></i>
+          </span>
+        </div>
+        :
+        <div>
+          <span>
+            <i className="shopping-cart">
+            </i>
+            {elements}
+          </span>
+        </div>
+      }
     </div>
-        
   );
 }
 
